a1dbdd377c274064d62ee18ca7d9516906d3baa5
chasehoward85 <chasehoward85@gmail.com>
2018-03-30 12:58:26 -0600

    Add files via upload

--- BattleShip.java
+++ BattleShip.java
@@ -121,7 +121,23 @@ public class BattleShip {
 							i = 0;
 						}
 						else{
-							p1Board[charPlace[0] - 64][charPlace[1] - 48] = '2';
+							if(charPlace[0] == test[0] && charPlace[1] == test[1] + 1){			//Row Same Column Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '2';
+							}
+							else if(charPlace[0] == test[0] && charPlace[1] == test[1] - 1){	//Row Same Column Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '2';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] + 1){	//Column Same Row Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '2';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] - 1){	//Column Same Row Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '2';
+							}
+							else{
+								System.out.println("Error:");
+								System.out.println("Row or Column input wrongly\n");
+								i = 0;
+							}
 						}						
 					}
 				}
@@ -130,7 +146,7 @@ public class BattleShip {
 		printP1Board();
 	}
 
-	public void placeThreeSpotShip(){
+	public void placeThreeSpotShip(){	//Done
 		Scanner kbReader = new Scanner(System.in);
 		
 		String place[] = new String[2];
@@ -176,13 +192,6 @@ public class BattleShip {
 						i = 0;
 					}
 					else{
-						/*if(charPlace[0] == test[0] && charPlace[1] == test[1] + 2 || 
-								charPlace[0] == test[0] && charPlace[1] == test[1] - 2){	//Row
-							
-						}
-						else if(charPlace[1] == test[1]){	//Column
-							
-						}*/
 						if(charPlace[0] > test[0] + 2 || charPlace[0] < test[0] - 2){	//Adjacent Row
 							System.out.println("Error:");
 							System.out.println("Row or Column input wrongly\n");
@@ -194,7 +203,27 @@ public class BattleShip {
 							i = 0;
 						}
 						else{
-							p1Board[charPlace[0] - 64][charPlace[1] - 48] = '3';
+							if(charPlace[0] == test[0] && charPlace[1] == test[1] + 2){			//Row Same Column Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '3';
+								p1Board[charPlace[0] - 64][charPlace[1] - 49] = '3';
+							}
+							else if(charPlace[0] == test[0] && charPlace[1] == test[1] - 2){	//Row Same Column Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '3';
+								p1Board[charPlace[0] - 64][charPlace[1] - 47] = '3';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] + 2){	//Column Same Row Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '3';
+								p1Board[charPlace[0] - 65][charPlace[1] - 48] = '3';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] - 2){	//Column Same Row Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '3';
+								p1Board[charPlace[0] - 63][charPlace[1] - 48] = '3';
+							}
+							else{
+								System.out.println("Error:");
+								System.out.println("Row or Column input wrongly\n");
+								i = 0;
+							}
 						}						
 					}
 				}
@@ -203,11 +232,187 @@ public class BattleShip {
 		printP1Board();
 	}
 	
-	public void placeFourSpotShip(){
+	public void placeFourSpotShip(){	//Done
+		Scanner kbReader = new Scanner(System.in);
 		
+		String place[] = new String[2];
+		char charPlace[] = new char[3];
+		
+		char test[] = new char[3];
+		
+		for(int i = 0; i < 2; i++){
+			if(i == 0){		//First Spot
+				System.out.println("What is the first place you would like your 4 long ship to be placed at?");
+				place[0] = kbReader.next().toUpperCase();
+				charPlace = place[0].toCharArray();
+				
+				if(charPlace[0] > 73 || charPlace[0] < 65 || charPlace.length > 2){	//On Board
+					System.out.println("Error:");
+					System.out.println("Row or Column not on board");
+					i = -1;
+				}
+				else if(p1Board[charPlace[0] - 64][charPlace[1] - 48] != ' '){
+					System.out.println("Error:");
+					System.out.println("Spot already taken");
+					i = -1;
+				}
+				else{
+					test = place[0].toCharArray();
+					p1Board[charPlace[0] - 64][charPlace[1] - 48] = '4';
+				}
+			}
+			else{			//Last Spot
+				System.out.println("What is the LAST place you would like your 4 long ship to be placed at?");
+				place[1] = kbReader.next().toUpperCase();
+				charPlace = place[1].toCharArray();
+				
+				if(charPlace[0] > 73 || charPlace[0] < 65 || charPlace.length > 2){	//On Board
+					System.out.println("Error:");
+					System.out.println("Row or Column not on board");
+					i = 0;
+				}
+				else{
+					if(charPlace[0] == test[0] && charPlace[1] == test[1]){
+						System.out.println("\nError:");
+						System.out.println("Row or Column input wrongly\n");
+						i = 0;
+					}
+					else{
+						if(charPlace[0] > test[0] + 3 || charPlace[0] < test[0] - 3){	//Adjacent Row
+							System.out.println("Error:");
+							System.out.println("Row or Column input wrongly\n");
+							i = 0;
+						}
+						else if(charPlace[1] > test[1] + 3 || charPlace[1] < test[1] - 3){	//Adjacent Column
+							System.out.println("Error:");
+							System.out.println("Row or Column input wrongly\n");
+							i = 0;
+						}
+						else{
+							if(charPlace[0] == test[0] && charPlace[1] == test[1] + 3){			//Row Same Column Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 64][charPlace[1] - 49] = '4';
+								p1Board[charPlace[0] - 64][charPlace[1] - 50] = '4';
+							}
+							else if(charPlace[0] == test[0] && charPlace[1] == test[1] - 3){	//Row Same Column Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 64][charPlace[1] - 47] = '4';
+								p1Board[charPlace[0] - 64][charPlace[1] - 46] = '4';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] + 3){	//Column Same Row Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 65][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 66][charPlace[1] - 48] = '4';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] - 3){	//Column Same Row Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 63][charPlace[1] - 48] = '4';
+								p1Board[charPlace[0] - 62][charPlace[1] - 48] = '4';
+							}
+							else{
+								System.out.println("Error:");
+								System.out.println("Row or Column input wrongly\n");
+								i = 0;
+							}
+						}						
+					}
+				}
+			}
+		}
+		printP1Board();
 	}
 	
 	public void placeFiveSpotShip(){
+		Scanner kbReader = new Scanner(System.in);
 		
+		String place[] = new String[2];
+		char charPlace[] = new char[3];
+		
+		char test[] = new char[3];
+		
+		for(int i = 0; i < 2; i++){
+			if(i == 0){		//First Spot
+				System.out.println("What is the first place you would like your 5 long ship to be placed at?");
+				place[0] = kbReader.next().toUpperCase();
+				charPlace = place[0].toCharArray();
+				
+				if(charPlace[0] > 73 || charPlace[0] < 65 || charPlace.length > 2){	//On Board
+					System.out.println("Error:");
+					System.out.println("Row or Column not on board");
+					i = -1;
+				}
+				else if(p1Board[charPlace[0] - 64][charPlace[1] - 48] != ' '){
+					System.out.println("Error:");
+					System.out.println("Spot already taken");
+					i = -1;
+				}
+				else{
+					test = place[0].toCharArray();
+					p1Board[charPlace[0] - 64][charPlace[1] - 48] = '5';
+				}
+			}
+			else{			//Last Spot
+				System.out.println("What is the LAST place you would like your 5 long ship to be placed at?");
+				place[1] = kbReader.next().toUpperCase();
+				charPlace = place[1].toCharArray();
+				
+				if(charPlace[0] > 73 || charPlace[0] < 65 || charPlace.length > 2){	//On Board
+					System.out.println("Error:");
+					System.out.println("Row or Column not on board");
+					i = 0;
+				}
+				else{
+					if(charPlace[0] == test[0] && charPlace[1] == test[1]){
+						System.out.println("\nError:");
+						System.out.println("Row or Column input wrongly\n");
+						i = 0;
+					}
+					else{
+						if(charPlace[0] > test[0] + 4 || charPlace[0] < test[0] - 4){	//Adjacent Row
+							System.out.println("Error:");
+							System.out.println("Row or Column input wrongly\n");
+							i = 0;
+						}
+						else if(charPlace[1] > test[1] + 4 || charPlace[1] < test[1] - 4){	//Adjacent Column
+							System.out.println("Error:");
+							System.out.println("Row or Column input wrongly\n");
+							i = 0;
+						}
+						else{
+							if(charPlace[0] == test[0] && charPlace[1] == test[1] + 4){			//Row Same Column Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 49] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 50] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 51] = '5';
+							}
+							else if(charPlace[0] == test[0] && charPlace[1] == test[1] - 4){	//Row Same Column Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 47] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 46] = '5';
+								p1Board[charPlace[0] - 64][charPlace[1] - 45] = '5';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] + 4){	//Column Same Row Increased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 65][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 66][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 66][charPlace[1] - 48] = '5';
+							}
+							else if(charPlace[1] == test[1] && charPlace[0] == test[0] - 4){	//Column Same Row Decreased
+								p1Board[charPlace[0] - 64][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 63][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 62][charPlace[1] - 48] = '5';
+								p1Board[charPlace[0] - 61][charPlace[1] - 48] = '5';
+							}
+							else{
+								System.out.println("Error:");
+								System.out.println("Row or Column input wrongly\n");
+								i = 0;
+							}
+						}						
+					}
+				}
+			}
+		}
+		printP1Board();
 	}
 }
--- Tester.java
+++ Tester.java
@@ -4,11 +4,14 @@ public class Tester {
 	public static void main(String[] args) {
 		
 		BattleShip test = new BattleShip();
-		
-		test.makeBoard();
-		//test.printBoard();
-		test.placeTwoSpotShip();
 
+		test.makeBoard();
+		//test.printEmptyBoard();
+		test.placeTwoSpotShip();
+		test.placeThreeSpotShip();
+		test.placeFourSpotShip();
+		test.placeFiveSpotShip();
+		
 	}
 
 }
